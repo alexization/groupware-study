7e71205e6951578fc189a2f1c5d6db83f708037c
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-08-08 13:56:04 +0900

    first commit

---
+++ src/test/java/com/ureca/urecaWare/UrecaWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.urecaWare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
